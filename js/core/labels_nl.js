17aad294ca783625baecc2c410ca67995ea42546
C. Wesselink <c.wesselink@taschestaalbouw.nl>
2026-03-17 09:25:21 +0100

    fase 11 rebuild lokaal veiligstellen

---
+++ js/core/labels_nl.js
@@ -0,0 +1,14 @@
+
+const labels={
+start:"Start",
+projecten:"Projecten",
+lassen:"Lassen",
+controle:"Controle",
+documenten:"Documenten",
+instellingen:"Instellingen",
+opslaan:"Opslaan",
+wijzigen:"Wijzigen",
+verwijderen:"Verwijderen",
+annuleren:"Annuleren",
+ce_export:"CE dossier exporteren"
+};
---
+++ js/modules/inspectie/inspectie_form.js
@@ -0,0 +1,6 @@
+
+function opslaanControle(){
+
+alert("Controle opgeslagen");
+
+}
